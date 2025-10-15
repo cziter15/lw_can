a9b6776d465101ed5798ae401737c4cfc9e6d095
Krzysztof Strehlau <chris.streh@gmail.com>
2025-10-15 17:18:02 +0200

    Remove unnecessary comment line in lw_can.cpp

--- src/lw_can.cpp
+++ src/lw_can.cpp
@@ -25,8 +25,8 @@
  * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
  */
+
 #include "lw_can.h"
 #include <stdint.h>
 #include <math.h>
